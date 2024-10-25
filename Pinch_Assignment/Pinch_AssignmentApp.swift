ee310fbd1ff70b21ec530cd038328cb630c5ff0a
Nups <nups543@gmail.com>
2024-10-25 18:21:55 +0530

    Initial Commit

---
+++ Pinch_Assignment/Pinch_AssignmentApp.swift
@@ -0,0 +1,32 @@
+//
+//  Pinch_AssignmentApp.swift
+//  Pinch_Assignment
+//
+//  Created by Nupur Sharma on 25/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Pinch_AssignmentApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Pinch_AssignmentTests/Pinch_AssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_AssignmentTests.swift
+//  Pinch_AssignmentTests
+//
+//  Created by Nupur Sharma on 25/10/24.
+//
+
+import Testing
+@testable import Pinch_Assignment
+
+struct Pinch_AssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
